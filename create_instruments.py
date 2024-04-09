4dd82ac66b747f89c8261d97e0c2881e3133ccb2
jayich lab <jayameena95@gmail.com>
2024-04-09 10:28:48 -0700

    Updated the commit

--- create_instruments.py
+++ create_instruments.py
@@ -26,36 +26,40 @@ def format_e(n):
 #%%
 if 1:
     trace_number = 1
-    measurement_format = "S21"
-    power = -15 #dBm
-    freq_start = 2e9 #Hz
-    freq_stop = 3e9 #Hz
-    if_bandwidth = 10e3 #10kHz
+    measurement_format = 'S11'
+    power = -45 #dBm
+    freq_start = 1e9 #Hz
+    freq_stop = 10e9 #Hz
+    if_bandwidth = 1e3 #1Hz
     instrument_name = "MyFieldFox"
-    instrument_address = "TCPIP1::192.168.1.120::inst0::INSTR"  # Replace with your instrument's VISA address
-
+    instrument_address = "TCPIP0::192.168.1.135::inst0::INSTR"  # Replace with your instrument's VISA address
+    num_points = 1601
+    power_level = -40
+    
     fieldfox = FieldFox(instrument_name, instrument_address)
 
-    # Example usage
-    fieldfox.create_measurement(1, "S21")
+    # # Example usage
+   #%%
     fieldfox.set_number_of_traces(1)
     fieldfox.set_measurement(1)
+    fieldfox.create_measurement(1, measurement_format)
     fieldfox.set_trace_format("MLOG")
-    fieldfox.split_display_window("D11")
-    fieldfox.perform_measurement_conversion("DBM")
-    
-    # Set the power level to 0 dBm (adjust the value as needed)
-    power_level = -15
-    fieldfox.set_source_power(f"SOURce:POWer {power_level} dBm")
+    # fieldfox.split_display_window("D1")[]
+    # fieldfox.set_start_frequency(freq_start)
+    # fieldfox.set_stop_frequency(freq_stop)
+    # fieldfox.perform_measurement_conversion("DBM")
+    # fieldfox.set_resolution(num_points)
+    # # Set the power level to 0 dBm (adjust the value as needed)
+    # fieldfox.set_source_power(f"SOURce:POWer {power_level} dBm")
 
-    # Query the power level to verify the setting
+    # # Query the power level to verify the setting
     result = fieldfox.query("SOURce:POWer?")
-    # print(result)
+    # # print(result)
     
-    # Generate plot title
-    plot_title = f"{measurement_format}_Trace{trace_number}_Power{power}_Freq{format_e(freq_start)}_{format_e(freq_stop)}_IFBW{format_e(if_bandwidth)}"
+    # # Generate plot title
+    plot_title = f"{measurement_format}_Power{power}"#"_Freq{format_e(freq_start)}_{format_e(freq_stop)}_IFBW{format_e(if_bandwidth)}"
     
-    print (plot_title)
+    # print (plot_title)
      #%%
     print(f"Power Level Set: {result} dBm")
     
