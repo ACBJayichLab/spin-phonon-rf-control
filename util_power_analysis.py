a56130f521bf8d59d3b7cc5238057570523b000c
Hyunseok Oh <hyunseok0222@gmail.com>
2024-07-16 13:24:17 -0700

    Updated FF

--- util_power_analysis.py
+++ util_power_analysis.py
@@ -3,41 +3,68 @@ import csv
 import os
 import numpy as np
 
-file_dir = './Data'
-file_header = "240411_cable 2_"
-power_lst = [-20, -25, -30, -35, -40, -45]
+file_dir = './Data/240418'
+# file_header = "240411_cable 2_"
+# power_lst = [-20, -25, -30, -35, -40, -45]
+file2 = "240418_150537_whole cables without device_S11.csv"
+file1 = "240418_134108_whole cables with device_S11.csv"
+
+
 
 fig, ax = plt.subplots()
 ax.set_xlabel("Frequency (GHz)")
 ax.set_ylabel("Magnitude (dB)")
-ax.set_title("VNA Measurement: S22")
+ax.set_title("VNA Measurement: S11")
 
-for power in power_lst:
-    file_name = file_header + str(power) + '.csv'
+with open(os.path.join(file_dir, file1), 'r') as fp:
+    reader = csv.reader(fp)
 
-    with open(os.path.join(file_dir, file_name), 'r') as fp:
-        reader = csv.reader(fp)
+    # Skip the first 31 lines
+    for _ in range(31):
+        next(reader)
 
-        # Skip the first 31 lines
-        for _ in range(31):
-            next(reader)
+    # Read the data until the "END" line
+    data_rows = []
+    x_values = []
+    y_values = []
+    
+    for row in reader:
+        if row and row[0].strip().upper() == "END":
+            break
+        data_rows.append(row)
+        x_values.append(float(row[0]))
+        y_values.append(float(row[1]))
 
-        # Read the data until the "END" line
-        data_rows = []
-        x_values = []
-        y_values = []
-        
-        for row in reader:
-            if row and row[0].strip().upper() == "END":
-                break
-            data_rows.append(row)
-            x_values.append(float(row[0]))
-            y_values.append(float(row[1]))
+    x_values = np.asarray(x_values)
+    y_values = np.asarray(y_values)
 
-        x_values = np.asarray(x_values)
-        y_values = np.asarray(y_values)
-        
-        ax.plot(x_values, y_values, label = f"{power} dBm")
+with open(os.path.join(file_dir, file2), 'r') as fp:
+    reader = csv.reader(fp)
+
+    # Skip the first 31 lines
+    for _ in range(31):
+        next(reader)
+
+    # Read the data until the "END" line
+    data_rows2 = []
+    x_values2 = []
+    y_values2 = []
+    
+    for row in reader:
+        if row and row[0].strip().upper() == "END":
+            break
+        data_rows2.append(row)
+        x_values2.append(float(row[0]))
+        y_values2.append(float(row[1]))
+
+    x_values2 = np.asarray(x_values2)
+    y_values2 = np.asarray(y_values2)
+ 
+
+
+ax.plot(x_values, y_values)
+ax.plot(x_values2, y_values2)
+ax.plot(x_values, y_values-y_values2)
     
 fig.legend()
 plt.show()
--- util_run_fieldfox.py
+++ util_run_fieldfox.py
@@ -5,32 +5,34 @@ from datetime import datetime
 import time
 
 # Save the plot with a timestamp in the filename
-timestamp = str(datetime.now().strftime("%y%m%d"))
-
+timestamp = str(datetime.now().strftime("%y%m%d_%H%M%S"))
+date = str(datetime.now().strftime("%y%m%d"))
 
 inst_name = "MyFieldFox"
-inst_addr = "TCPIP0::192.168.1.135::inst0::INSTR"  # Replace with your instrument's VISA address
+inst_addr = "TCPIP0::192.168.1.134::inst0::INSTR"  # Replace with your instrument's VISA address
 
 trace_number = 1
-measurement = "S11"
-power = -45 # dBm
+measurement = "S12"
+power = -20 # dBm
 freq_start = 2.5e9 # Hz
-freq_stop = 3.0e9 # Hz
-num_points = 601
+freq_stop = 3e9 # Hz
+num_points = 1001
 avg_number = 50
 wait_time = 10 # seconds
 
+root_dir = './Data'
+file_dir = os.path.join(root_dir, date)
 
+if not os.path.isdir(file_dir):
+    os.mkdir(file_dir)
 
-
-file_dir = './Data'
-file_name = timestamp + '_cable 2_'+str(power)+'.csv'
+file_name = timestamp + '_direct device_'+str(measurement)+'.csv'
 file_path = os.path.join(file_dir, file_name)
 
 
 # Set up the FieldFox VNA
 ff = FieldFox(inst_name, inst_addr)
-ff.set_mode("NA")
+# ff.set_mode("NA")
 ff.set_measurement(trace_number, measurement)
 ff.set_trace_format("MLOGarithmic")
 ff.set_start_frequency(freq_start)
@@ -41,7 +43,7 @@ power_read = float(ff.query("SOURce:POWer?"))
 print(f"Power level: {power_read:.2f} dBm")
 ff.set_avg_number(avg_number)
 
-# Wait for the measurement to complete
+# # Wait for the measurement to complete
 time.sleep(wait_time)
 
 # Save the data to a CSV file
@@ -53,8 +55,9 @@ ff.set_source_power(-45)
 
 # Plot
 fig, ax = plt.subplots()
-ax.plot(xdata/1e9, ydata)
+ax.plot(xdata/1e9, ydata, label = f"{power} dBm")
 ax.set_xlabel("Frequency (GHz)")
 ax.set_ylabel("Magnitude (dB)")
 ax.set_title(f"VNA Measurement: {measurement}\n{file_path}")
+fig.legend()
 plt.show()
